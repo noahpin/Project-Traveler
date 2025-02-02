bb3176cbde6e0c206d10ddc8ee05d6e0fc486a10
Noah Pinales <pinales@usc.edu>
2025-02-01 17:43:31 -0800

    new isr test

--- src/routes/(public)/[...slug]/+page.server.ts
+++ src/routes/(public)/[...slug]/+page.server.ts
@@ -1,4 +1,4 @@
-export const prerender = 'auto'; // Prerenders at build time, but allows new posts via SSR
+// export const prerender = 'auto'; // Prerenders at build time, but allows new posts via SSR
 export const ssr = true;
 import { error } from '@sveltejs/kit';
 import type { EntryGenerator, PageServerLoad } from './$types';
